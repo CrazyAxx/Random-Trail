496680b79f2ac916ad1b3de0596d0ea4e632d97b
Collin Bates <244503@bwls.net>
2016-10-28 08:22:09 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,12 @@
 function setup() {
-
+  createCanvas( windowWidth , windowHeight );
 }
 
 function draw() {
-
-}
+  var r = random( 0 , 255 );
+  var g = random( 0 , 255 );
+  var b = random( 0 , 255 );
+  var a = random( 0 , 75 )
+  fill( r , g , b , a );
+  ellipse( mouseX , mouseY , 50 , 50 );
+}s
